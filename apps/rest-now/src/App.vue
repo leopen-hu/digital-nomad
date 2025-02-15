30f7609a5a190d6db742c0e82eeedd8d7de4a5c1
leopen <790480953@qq.com>
2025-02-15 12:46:18 +0800

    feat: add support for shadcn-vue

--- apps/rest-now/src/App.vue
+++ apps/rest-now/src/App.vue
@@ -1,8 +1,29 @@
-<template>
-  <h1>💖 Hello World!</h1>
-  <p>Welcome to your Electron application.</p>
-</template>
-
-<script setup>
-console.log('👋 This message is being logged by "App.vue", included via Vite');
+<script setup lang="ts">
+import {
+  Menubar,
+  MenubarContent,
+  MenubarItem,
+  MenubarMenu,
+  MenubarSeparator,
+  MenubarShortcut,
+  MenubarTrigger,
+} from '@/components/ui/menubar'
 </script>
+
+<template>
+  <Menubar>
+    <MenubarMenu>
+      <MenubarTrigger>File</MenubarTrigger>
+      <MenubarContent>
+        <MenubarItem>
+          New Tab <MenubarShortcut>⌘T</MenubarShortcut>
+        </MenubarItem>
+        <MenubarItem>New Window</MenubarItem>
+        <MenubarSeparator />
+        <MenubarItem>Share</MenubarItem>
+        <MenubarSeparator />
+        <MenubarItem>Print</MenubarItem>
+      </MenubarContent>
+    </MenubarMenu>
+  </Menubar>
+</template>
--- apps/rest-now/src/renderer.ts
+++ apps/rest-now/src/renderer.ts
@@ -28,6 +28,5 @@
 
 import { createApp } from 'vue'
 import App from './App.vue'
-import './index.css'
 
 createApp(App).mount('#app')
