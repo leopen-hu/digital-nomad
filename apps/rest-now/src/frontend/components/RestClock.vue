48a41d5e0648679f2193e2ce6ef88919b4e5a8cb
leopen <790480953@qq.com>
2025-02-22 15:35:00 +0800

    feat: add clock for rest (only interface)

---
+++ apps/rest-now/src/frontend/components/RestClock.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+import { DotFilledIcon } from '@radix-icons/vue'
+</script>
+
+<template>
+  <div class="flex flex-row flex-nowrap select-none">
+    <div class="flex items-center justify-center">
+      <span class="text-digital-clock text-6xl">00</span>
+    </div>
+    <div class="flex flex-col items-center justify-center">
+      <DotFilledIcon class="digital-clock-dot w-6 h-6 mx-1" />
+      <DotFilledIcon class="digital-clock-dot w-6 h-6 mx-1" />
+    </div>
+    <div class="flex items-center justify-center">
+      <span class="text-digital-clock  text-6xl">00</span>
+    </div>
+  </div>
+</template>
--- apps/rest-now/src/frontend/pages/Rest.vue
+++ apps/rest-now/src/frontend/pages/Rest.vue
@@ -1,15 +1,19 @@
 <script setup lang="ts">
-import { Button } from '../components/ui/button';
+import { Button } from '../components/ui/button'
+import RestClock from '@/frontend/components/RestClock.vue'
 
 const returnToWork = () => {
-  window.restApi.close();
-};
+  window.restApi.close()
+}
 </script>
 
 <template>
-  <div class="flex h-dvh overflow-x-hidden items-center justify-center bg-black text-white">
-    <Button variant="ghost" @click="returnToWork">
-      Return To Work
+  <div
+    class="flex flex-col h-dvh overflow-x-hidden items-center justify-center bg-black text-white"
+  >
+    <RestClock />
+    <Button variant="link" @click="returnToWork" class="mt-4 text-gray-300 text-base tracking-widest select-none">
+      Back To Work
     </Button>
   </div>
 </template>
