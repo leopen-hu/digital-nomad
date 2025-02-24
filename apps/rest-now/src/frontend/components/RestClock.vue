40d92dbd7a58abdc12566b75bf9428fba6c71133
leopen <790480953@qq.com>
2025-02-24 08:12:50 +0800

    fix: rest time was not recorded when computer slept

--- apps/rest-now/src/frontend/components/RestClock.vue
+++ apps/rest-now/src/frontend/components/RestClock.vue
@@ -1,19 +1,15 @@
 <script setup lang="ts">
 import { onMounted, onUnmounted, ref } from 'vue'
 import { DotFilledIcon } from '@radix-icons/vue'
-import formatSeconds from '@/common/format-seconds'
+import formatDuration from '@/common/format-duration'
 
 let timer: NodeJS.Timeout
-const time = ref(0)
-const formattedTime = ref(formatSeconds(0))
+const formattedTime = ref(formatDuration(0))
 
 onMounted(() => {
+  const startAt = Date.now()
   timer = setInterval(() => {
-    time.value++
-    formattedTime.value = formatSeconds(time.value)
-    if (time.value >= 3600) {
-      clearInterval(timer)
-    }
+    formattedTime.value = formatDuration(Date.now() - startAt)
   }, 1000)
 })
 
@@ -24,6 +20,13 @@ onUnmounted(() => {
 
 <template>
   <div class="flex flex-row flex-nowrap select-none">
+    <div class="flex items-center justify-center">
+      <span class="text-digital-clock text-6xl">{{ formattedTime.hours }}</span>
+    </div>
+    <div class="flex flex-col items-center justify-center">
+      <DotFilledIcon class="digital-clock-dot w-6 h-6 mx-1" />
+      <DotFilledIcon class="digital-clock-dot w-6 h-6 mx-1" />
+    </div>
     <div class="flex items-center justify-center">
       <span class="text-digital-clock text-6xl">{{ formattedTime.minutes }}</span>
     </div>
